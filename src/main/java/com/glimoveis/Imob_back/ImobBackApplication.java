acff8ade38b8fd061373c11e9f5a78e60a890b91
Gabriel Alves <gabrieldamasceno881@outlook.com>
2023-12-01 13:05:19 -0300

    feat: Starting project

---
+++ src/main/java/com/glimoveis/Imob_back/ImobBackApplication.java
@@ -0,0 +1,13 @@
+package com.glimoveis.Imob_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glimoveis/Imob_back/ImobBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glimoveis.Imob_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
