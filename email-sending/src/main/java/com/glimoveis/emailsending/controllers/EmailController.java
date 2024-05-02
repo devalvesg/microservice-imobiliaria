36311232e1c3c8d416df24f22f17e78a0879fb6c
Gabriel Alves <gabrieldamasceno881@outlook.com>
2024-05-02 14:59:25 -0300

    feat: Criação de endpoint para uso de cronjob

--- email-sending/src/main/java/com/glimoveis/emailsending/controllers/EmailController.java
+++ email-sending/src/main/java/com/glimoveis/emailsending/controllers/EmailController.java
@@ -6,6 +6,7 @@ import com.glimoveis.emailsending.services.EmailService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,10 @@ public class EmailController {
         emailService.envioBoasVindas(emailModel, userResponse.name());
 
         return ResponseEntity.status(HttpStatus.CREATED).body("Email de boas vindas disparado");
+    }
+
+    @GetMapping("emails/sleep")
+    public ResponseEntity dontSleepApi(){
+        return ResponseEntity.ok().body("");
     }
 }
