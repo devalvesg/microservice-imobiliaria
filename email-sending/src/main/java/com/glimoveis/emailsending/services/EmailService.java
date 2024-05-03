78c6975b02b1daf77e00e841a99b35fda4e8e3d0
Gabriel Alves <gabrieldamasceno881@outlook.com>
2024-05-03 13:49:51 -0300

    fix: Correção na falha ao enviar novos emails

--- email-sending/src/main/java/com/glimoveis/emailsending/services/EmailService.java
+++ email-sending/src/main/java/com/glimoveis/emailsending/services/EmailService.java
@@ -3,13 +3,19 @@ package com.glimoveis.emailsending.services;
 import com.glimoveis.emailsending.models.EmailModel;
 import com.glimoveis.emailsending.models.StatusEmail;
 import com.glimoveis.emailsending.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.Properties;
 
 @Service
 public class EmailService {
+
     private EmailRepository emailRepository;
     private JavaMailSender javaMailSender;
 
@@ -23,17 +29,21 @@ public class EmailService {
         final String assunto = "Obrigado por fazer cadastro em nosso site";
         final String conteudo = "Olá " + nomeUsuario + " ficamos felizes que você tenha feito cadastro em nosso site. Fique a vontade e aproveite as melhores casas de Franca!";
 
+
         try{
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom(emailRemetente);
             message.setTo(emailModel.getEmailDestinatario());
             message.setSubject(assunto);
             message.setText(conteudo);
-            javaMailSender.send(message);
 
             emailModel.setEmailRemetente(emailRemetente);
             emailModel.setAssunto(assunto);
             emailModel.setConteudo(conteudo);
+
+            javaMailSender.send(message);
+
+
             emailModel.setStatusEmail(StatusEmail.ENVIADO);
         }catch (MailException e){
             emailModel.setStatusEmail(StatusEmail.ERRO);
@@ -41,4 +51,7 @@ public class EmailService {
             emailRepository.save(emailModel);
         }
     }
+
+
+
 }
